4ffcbcbcafa049fab5a8eeef3234dd19628745c1
Ramasamy Ramanathan <53431872+RM-RAMASAMY@users.noreply.github.com>
2025-04-02 18:16:49 -0700

    Update test_gemini.py

--- test_gemini.py
+++ test_gemini.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyBk3xJKy3vwKQcrcaz1hBdV9sQZN5wkzPk")
+client = genai.Client(api_key=<YOUR_API_KEY>)
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works in a a lot of words"
